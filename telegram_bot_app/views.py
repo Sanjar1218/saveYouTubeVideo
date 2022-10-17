b625d8a4067d339603d22951f5e54799f750ac74
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-17 13:20:22 +0500

    webhook

--- telegram_bot_app/views.py
+++ telegram_bot_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# django libraries
+from django.http import JsonResponse
 
-# Create your views here.
+# python telegram bot libraries
+import telegram
+
+TOKEN = '5128554563:AAGXrWFZ8YKt0lSbFpr6X1xo4P14X-L5NjE'
+url = 'site to be run'
+
+def setWeebHook(request):
+    # for more information about webhook head over to this site
+    #https://docs.python-telegram-bot.org/en/stable/telegram.bot.html#telegram.Bot.set_webhook
+    
+    bot = telegram.Bot(token=TOKEN)
+    bot.setWebhook(url)
+    return JsonResponse({'status':bot.getWebhookInfo()})
+
+def home(request):
+    #telegram bot handlers here
+
+    return JsonResponse({'status':'ok'})
